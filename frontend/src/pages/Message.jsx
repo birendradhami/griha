47df327a312b10a254f8f27ca5a9f436dd9232d8
masskalauni <masskalauni@gmail.com>
2024-03-02 12:47:18 +0545

    tested chat

--- frontend/src/pages/Message.jsx
+++ frontend/src/pages/Message.jsx
@@ -88,85 +88,75 @@ const Message = () => {
                   </svg>
                 </span>
 
-                                    <span className="text-sm font-medium transition-all  group-hover:me-4">
-                                        Login
-                                    </span>
-                                </button>
-                            </p>
+                <span className="text-sm font-medium transition-all  group-hover:me-4">
+                  Login
+                </span>
+              </button>
+            </p>
+          </div>
+        ) : (
+          <div>
+            {conversations.length === 0 ? (
+              <div>
+                <p className="bg-white text-center text-sm sm:text-2xl mt-20 mb-10 font-heading font-bold flex flex-col items-center justify-center max-w-3xl mx-auto py-10 text-black px-5 rounded shadow-md">
+                  <span> No messages to display. Start a conversation!</span>
+                </p>
+              </div>
+            ) : (
+              <div className="chats_container custom_scrollbar grid grid-cols-12 ">
+                <div className="chat_people_container bg-white col-span-3  py-4 sm:py-5 flex sm:items-start items-center justify-start flex-col gap-2 overflow-y-scroll ">
+                  {conversationLoading ? (
+                    <div className="w-full">
+                      <p className="text-center mt-10 font-bold font-heading text-sm text-black">
+                        Conversations Loading
+                      </p>
+                    </div>
+                  ) : (
+                    <>
+                      <h3 className=" mx-auto px-2 mb-3 sm:px-3 text-sm sm:text-[22px] font-medium text-black">
+                        Conversations
+                      </h3>
+                      {conversations.length !== 0 &&
+                        conversations.map((conversation) => (
+                          <Conversations
+                            conversationInfo={{
+                              conversation,
+                              trackConversation,
+                              setTrackConversation,
+                            }}
+                            key={conversation._id}
+                          />
+                        ))}
+                    </>
+                  )}
+                </div>
 
-                        </div>
-                        :
-                        <div>
-                            {
-                                conversations.length === 0 ?
-                                    <div>
-
-                                        <p className='bg-white text-center text-sm sm:text-2xl mt-20 font-heading font-bold flex flex-col items-center justify-center max-w-3xl mx-auto py-10 text-black px-5 rounded shadow-md'>
-                                            <span> Hey welcome! &#x1F604;</span>
-                                            <span> This is a blank canvas waiting for your first conversation.</span>
-                                        </p>
-
-                                    </div>
-                                    :
-                                    <div className="chats_container custom_scrollbar grid grid-cols-12 ">
-
-                                        <div className="chat_people_container bg-white col-span-3  py-4 sm:py-5 flex sm:items-start items-center justify-start flex-col gap-2 overflow-y-scroll ">
-
-                                            {
-                                                conversationLoading
-                                                    ?
-                                                    <div className='w-full'>
-                                                        <p className='text-center mt-10 font-bold font-heading text-sm text-black'>Conversation Loading...</p>
-                                                    </div>
-                                                    :
-                                                    <>
-                                                        <h3 className='font-heading  px-2 mb-3 sm:px-3 text-sm sm:text-3xl text-black'>Chats...</h3>
-                                                        {
-                                                            conversations.length !== 0 &&
-                                                            conversations.map((conversation) =>
-                                                                <Conversations
-                                                                    conversationInfo={
-                                                                        {
-                                                                            conversation,
-                                                                            trackConversation,
-                                                                            setTrackConversation,
-                                                                        }
-                                                                    }
-                                                                    key={conversation._id}
-                                                                />
-                                                            )
-
-                                                        }
-                                                    </>
-                                            }
-                                        </div>
-
-                                        {
-                                            trackConversation.conversationActive
-                                                ?
-                                                <div className="conversation_container col-span-9 ">
-                                                    <Chat conversationInfo={{
-                                                        trackConversation,
-                                                        setTrackConversation,
-                                                        conversations,
-                                                        setConversation,
-                                                    }} />
-                                                </div>
-                                                :
-                                                <div className="conversation_container col-span-9 ">
-
-                                                    <p className='mt-20 text-sm sm:text-2xl text-center font-heading '>No Conversation is Selected 	&#128580;</p>
-                                                </div>
-                                        }
-                                    </div>
-                            }
-                        </div>
-                }
-            </section>
-
-
-        </>
-    )
-}
+                <div
+                  className="conversation_container col-span-9"
+                  ref={chatContainerRef}
+                >
+                  {trackConversation.conversationActive ? (
+                    <Chat
+                      conversationInfo={{
+                        trackConversation,
+                        setTrackConversation,
+                        conversations,
+                        setConversation,
+                      }}
+                    />
+                  ) : (
+                    <p className="mt-20 text-sm sm:text-2xl font-medium text-center text-black ">
+                      Select and Start Conversation!
+                    </p>
+                  )}
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </section>
+    </>
+  );
+};
 
 export default Message;
