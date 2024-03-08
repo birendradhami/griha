97172e18966075e846d2d51761b85c8d94220ff4
Birendra Dhami <sbirendradhami@gmail.com>
2024-03-08 10:59:57 +0545

    fix proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://grihabackend.onrender.com',
+        changeOrigin: true,
         secure: false,
       },
     },
