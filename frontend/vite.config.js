936239ef34feef3e7864ee5b7dc325b7a6c142cc
Birendra Dhami <sbirendradhami@gmail.com>
2024-03-08 11:18:20 +0545

    fix vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://grihabackend.onrender.com',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+  export default defineConfig({
+    plugins: [
+      react(),
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'https://grihabackend.onrender.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
 
   plugins: [react()],
 });
